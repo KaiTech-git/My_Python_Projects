9d5cd006b7247f806273cf94d3a96d8e35d3d4b2
Kaj <69725937+KaiTech-git@users.noreply.github.com>
2024-03-08 09:32:50 +0000

    Error fix in recurrent program. Creation iteration program.

---
+++ Hanoi Problem/HanoiTower_iter.py
@@ -0,0 +1,42 @@
+
+from HanoiTower_recur import Tower
+
+def element_transfer (tower1:Tower, tower2:Tower):
+    if tower1.top_disk() > tower2.top_disk():
+        tower1.add(tower2.pop())
+    else:
+        tower2.add(tower1.pop())
+    
+def hanoi_alg_iter(begin:Tower, temp:Tower, end:Tower):
+    tw_hight = len(begin.elements)
+    while len(end.elements) != tw_hight:
+        if tw_hight % 2 == 0:
+            tw_list=[(begin, temp), (begin, end), (temp, end)]
+            for tw1, tw2 in tw_list:
+                element_transfer(tw1, tw2)
+                if len(end.elements) == tw_hight:
+                    break
+
+        else:
+            tw_list=[(begin, end), (begin, temp), (temp, end)]
+            for tw1, tw2 in tw_list:
+                element_transfer(tw1, tw2)
+                if len(end.elements) == tw_hight:
+                    break            
+              
+                        
+
+    
+        
+if __name__ == "__main__":
+        tw_begin = Tower(7)
+        tw_temp = Tower(0) 
+        tw_end = Tower(0)
+        
+        hanoi_alg_iter(tw_begin, tw_temp, tw_end)
+        
+        print(tw_begin)
+        print(tw_temp)
+        print(tw_end)
+        
+        
--- Hanoi Problem/HanoiTower_recur.py
+++ Hanoi Problem/HanoiTower_recur.py
@@ -2,38 +2,43 @@
 class Tower():
     def __init__(self,n_elem:int = 0):
         if n_elem > 0:
-            self.elements = [x for x in range(1,n_elem+1)]
+            self.elements = [x for x in range(1,n_elem+1)][::-1]
         else:
             self.elements=[]
-    def pop(self,elm=-1):
-        return self.elements.pop(elm)
+    def pop(self):
+        return self.elements.pop()
     def add(self,element):
         self.elements.append(element)
+    def top_disk(self):
+        try:
+            return self.elements[-1]
+        except:
+            return 100
     def __str__(self):
         return str(self.elements)
     
-def hanoi_alg(begin:Tower, temp:Tower, end:Tower, n:int):
+def hanoi_alg_rec(begin:Tower, temp:Tower, end:Tower, n:int):
     
     if n == 1:
         end.add(begin.pop())
 
     else:
-        hanoi_alg(begin = begin,temp = end,end = temp, n=n-1)
+        hanoi_alg_rec(begin = begin,temp = end,end = temp, n=n-1)
     
         end.add(begin.pop())
         
-        hanoi_alg(begin = temp,temp = begin,end = end, n= n-1)
+        hanoi_alg_rec(begin = temp,temp = begin,end = end, n=n-1)
 
         return
 
     
         
 if __name__ == "__main__":
-        tw_begin = Tower(6)
+        tw_begin = Tower(10)
         tw_temp = Tower(0) 
         tw_end = Tower(0)
         
-        hanoi_alg(tw_begin, tw_temp, tw_end, len(tw_begin.elements))
+        hanoi_alg_rec(tw_begin, tw_temp, tw_end, len(tw_begin.elements))
         
         print(tw_begin)
         print(tw_temp)
